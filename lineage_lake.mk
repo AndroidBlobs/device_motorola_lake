fe3482a93c4c950bd1c659647da83cbacc7d529d
ShivamKumarJha <jha.shivam3@gmail.com>
2020-02-11 04:26:23 +0000

    10 QPW30.61-21 cb81e release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ lineage_lake.mk
@@ -0,0 +1,23 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from lake device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := motorola
+PRODUCT_DEVICE := lake
+PRODUCT_MANUFACTURER := motorola
+PRODUCT_NAME := lineage_lake
+PRODUCT_MODEL := moto g(7) plus
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+TARGET_VENDOR := motorola
+TARGET_VENDOR_PRODUCT_NAME := lake
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC=" 10 QPW30.61-21 cb81e release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := motorola/lake_retail/lake:10/QPW30.61-21/cb81e:user/release-keys
